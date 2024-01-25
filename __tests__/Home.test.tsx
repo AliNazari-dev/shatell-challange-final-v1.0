d4a82e81bfe8de189115b768d64ae1b38355e7ca
Ali <AliNazarii7394@gmail.com>
2024-01-25 19:11:41 +0330

    starting shatell challange

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,11 @@
+import Home from "@/src/app/page";
+import { render, screen } from "@testing-library/react";
+
+test("renders home component with a button", () => {
+  render(<Home />);
+  const buttonElement = screen.getByRole("button", { name: /click on me/i });
+
+  // Check if the button is in the document
+  expect(buttonElement).toBeInTheDocument();
+
+});
